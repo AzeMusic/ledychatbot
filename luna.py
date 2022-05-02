e5c81b48669f42b52f501d6514b90199363fbd00
mragasupport <99508889+AzeMusic@users.noreply.github.com>
2022-05-02 17:42:58 +0400

    Update luna.py

--- luna.py
+++ luna.py
@@ -53,8 +53,8 @@ async def type_and_send(message):
 @luna.on_message(filters.command("repo") & ~filters.edited)
 async def repo(_, message):
     await message.reply_text(
-        "[GitHub](https://github.com/thehamkercat/LunaChatBot)"
-        + " | [Group](t.me/PatheticProgrammers)",
+        "[ledy robot](https://t.me/ledyplaylist)"
+        + " | [Support](t.me/SOQrup)",
         disable_web_page_preview=True,
     )
 
@@ -63,7 +63,7 @@ async def repo(_, message):
 async def start(_, message):
     await luna.send_chat_action(message.chat.id, "typing")
     await sleep(2)
-    await message.reply_text("/repo - Get Repo Link")
+    await message.reply_text("salam, mən ledy söhbət botuyam sahibim @Tenha055 ✨")
 
 
 @luna.on_message(
@@ -109,7 +109,7 @@ async def main():
     print(
         """
 -----------------
-| Luna Started! |
+| Ledy Chat Bot Started! |
 -----------------
 """
     )
